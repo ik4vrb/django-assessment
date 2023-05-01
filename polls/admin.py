6263c7805933e03f8b16cbba5a2822459a61426c
ik4vrb <123338337+ik4vrb@users.noreply.github.com>
2023-05-01 18:11:51 -0400

    Add files via upload

---
+++ polls/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import Choice, Question, DeepThought
+
+# Register your models here.
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields':['question_text']}),
+        ('Dete information', {'fields':['pub_date']}),
+    ]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(DeepThought)
